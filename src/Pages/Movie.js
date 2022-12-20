710463908075474b836943512ec10995bf89c20b
magbha <abdelghanibha@gmail.com>
2022-12-20 12:09:59 +0000

    update

--- src/Pages/Movie.js
+++ src/Pages/Movie.js
@@ -1,17 +1,25 @@
 import React from "react";
-import { useParams } from "react-router-dom";
-import './Movie.css';
+import { useNavigate, useParams } from "react-router-dom";
+import "./Movie.css";
 import MovieCard from "../Components/MovieCard/MovieCard";
+import { Button } from "react-bootstrap";
 
 const Movie = ({ movies }) => {
+  const navigate = useNavigate()
   const { movieId } = useParams();
   return (
     <div className="card-section">
-      {movies
-        .filter(card => card.id == movieId)
-        .map((card, index) => (
-        <MovieCard className="moviecard" movie={card} key={index} />
-      ))}
+      <div className="deCard">
+        <div className="desCard">
+          {movies
+            .filter((card) => card.id == movieId)
+            .map((card, index) => (
+              <p key={index}>{card.description}</p>
+            ))}
+        </div>
+        <Button variant="warning" onClick={() => navigate(-1)}>Go Back to Previous Page </Button>
+
+      </div>
     </div>
   );
 };
